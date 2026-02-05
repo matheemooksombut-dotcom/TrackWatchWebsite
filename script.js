e92923f921c95d52e5f34eb921cd5b76d031c911
matheemooksombut-dotcom <matheemooksombut@gmail.com>
2026-02-05 19:03:17 +0700

    font

---
+++ script.js
@@ -0,0 +1,13 @@
+    document.addEventListener("DOMContentLoaded", function() {
+            const observer = new IntersectionObserver((entries) => {
+                entries.forEach(entry => {
+                    if (entry.isIntersecting) {
+                        entry.target.classList.add('show');
+                        observer.unobserve(entry.target);
+                    }
+                });
+            }, { threshold: 0.2 });
+
+            const targets = document.querySelectorAll('.img-content3 img, .content-pic-1 img, .img-insidesportcontent-2 img');
+            targets.forEach(target => observer.observe(target));
+        });
