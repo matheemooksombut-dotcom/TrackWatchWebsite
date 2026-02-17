40eae57f55d8984af80db5d8e46c92f309337278
matheemooksombut-dotcom <matheemooksombut@gmail.com>
2026-02-17 21:04:35 +0700

    frontScript

--- script.js
+++ script.js
@@ -11,3 +11,26 @@
             const targets = document.querySelectorAll('.img-content3 img, .content-pic-1 img, .img-insidesportcontent-2 img, .img-product img ');
             targets.forEach(target => observer.observe(target));
         });
+
+
+const changeimgProduct = document.querySelector('.change-product')
+const dots  = document.querySelectorAll('.dot')
+
+
+// Product Image
+const colorImage={
+    white: 'Template/whitewatch.png', 
+    black: 'Template/showcasewatch.png',
+    blue: 'Template/bluewatch.png',
+    purple: 'Template/purplewatch.png', 
+    green: 'Template/greenwatch.png'
+}
+
+
+dots.forEach(dot =>{
+    dot.addEventListener('click', function(){
+        const color = dot.dataset.color
+        changeimgProduct.src = colorImage[color]
+    })
+})
+
