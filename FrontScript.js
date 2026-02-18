3d1a7a86d932bb6ca7ca411e8887fd43817fe264
matheemooksombut-dotcom <matheemooksombut@gmail.com>
2026-02-18 21:38:32 +0700

    ๊Updated

--- FrontScript.js
+++ FrontScript.js
@@ -1,3 +1,30 @@
+const { text } = require("body-parser")
+
+// Connecting Form
+document.addEventListener("DOMContentLoaded",()=>{
+  const RegisterForm = document.querySelector('#registerForm')
+  const Loginform = document.querySelector('#loginForm')
+
+
+
+  if(RegisterForm){
+    RegisterForm.addEventListener("submit" , async(e)=>{
+      e.preventDefault()
+      await submitRegister()
+
+    })
+  }
+  if(Loginform){
+    Loginform.addEventListener("submit" , async(e)=>{
+      e.preventDefault()
+      await submitLogin()
+
+    });
+  }
+})
+
+
+
 
 
 
@@ -7,13 +34,42 @@ const submitRegister = async () =>{
     let UsernameUserDom = document.querySelector('input[name="Username"]')
     let PasswordUserDom = document.querySelector('input[name="Password"]')
     let ConfirmPasswordUserDom = document.querySelector('input[name="ConfrimePassword"]')
-    
+     const strongPassword = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[^A-Za-z\d]).{6,}$/;
+    const thaiPattern = /[\u0E00-\u0E7F]/;
+
+    if (thaiPattern.test(PasswordDom.value)) {
+        Swal.fire({
+            title: 'Invalid Input',
+            text: 'Password cannot contain Thai characters',
+            icon: 'warning'
+        });
+        return;
+    }
+
+
+
+    if (PasswordDom.value !== ConfirmPasswordDom.value) {
+            Swal.fire({
+            title: 'Password Error',
+            text: 'Password not match',
+            icon: 'warning'
+        });
+        return;
+    }  
+    if (!strongPassword.test(PasswordDom.value)) {
+        Swal.fire({
+        title: 'Password Error',
+        text: ' Password must contain letters, numbers and at least 6 characters and Special characters',
+        icon: 'warning'
+    });
+    return;
+    }
   try{
 
     let userData = {
-        Fullname: UsernameUserDom.value,
-        Email: EmailUserDom.value,
-        Password: PasswordUserDom.value
+        Username: UsernameUserDom.value,
+        Password: PasswordUserDom.value,
+        Email: EmailUserDom.value
     }
     const response = await axios.post(
         'http://localhost:3000/user',userData
@@ -27,15 +83,55 @@ const submitRegister = async () =>{
     })
     console.log(response.data)
 
-
-    
-
-
-
   }catch(error){
     console.log(error)
   }
 }
+
+// Login Section
+const submitLogin = async () =>{ 
+  let UsernameDom = document.querySelector('input[name="Username"]')
+  let PasswordDom = document.querySelector('input[name="Password"]')
+
+
+  if(UsernameDom.value === '' || PasswordDom.value === ''){
+      Swal.fire({
+        title: 'Please Enter Username and Password',
+        text: 'Try Again',
+        icon: 'warning'
+      })
+      return; 
+  }
+
+  try{
+    await axios.post('http://localhost:3000/login',{
+      Username: UsernameDom.value,
+      Password: PasswordDom.value
+    })
+
+    Swal.fire({
+      title: 'Login Success' , 
+      icon: 'success',
+      
+  }).then(()=>{
+      window.location.href = '../UserSection/index.html'; 
+  })
+
+    
+
+  }catch(error){
+    console.log(error)
+
+    Swal.fire({
+      title: 'Login Failed' , 
+      icon: 'error',
+      
+  })
+  }
+}
+
+
+
 
 
 
--- server/server.js
+++ server/server.js
@@ -2,7 +2,9 @@ const express  = require('express')
 const bodyParser = require('body-parser')  
 const cors  =  require('cors') 
 const app = express()
-const PORT = 3000|| process.env.PORT; 
+const mysql = require('mysql2/promise')
+const PORT = process.env.PORT || 3000;
+
 
 
 app.use(bodyParser.json())
@@ -18,9 +20,70 @@ app.get('/users',  (req , res) =>{
     res.send('Hello World')
 })
 
-//   ส่ง  User เข้า Database
-app.post('/user' , (raq, res) =>{
+
+// Connecting Database
+const pool = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'trackwatchwebsite' , 
+    port: 3306
+
+})
+
+//   Register
+app.post('/user' , async(req, res) =>{
     
+    try{
+        const{Username , Email , Password } = req.body;
+        const [result] = await pool.query(
+              `INSERT INTO users ( Username, Password , Email)
+             VALUES (?, ?, ? )`,
+            [Username, Password , Email]
+        );
+        res.json({
+            message: "User Created",
+            id: result.insertId
+        })
+
+    } catch(err){
+        console.log(err)
+        res.status(500).send('Internal Server Error')
+    }
+
+})
+
+// Login 
+app.post('/login' , async(req, res) =>{
+
+    try{
+
+        const { Username , Password} = req.body;
+        
+        const [users] = await pool.query(
+            'SELECT * FROM users WHERE Username = ? AND Password = ?',
+            [Username, Password]
+        
+        );
+        if(users.length === 0){
+            return res.status(401).json({ message: 'User not found'})
+        }
+        const user = users[0]
+
+        if(user.Password !== Password){
+            return res.status(401).json({ message: 'Invalid Password'})
+
+        }
+        res.json({message: 'Login Success'})
+
+
+
+
+
+    }catch(err){
+        console.log(err)
+        res.status(500).send('Internal Server Error')
+    }
 })
 
 
