5ff3da5c57a47fe4b92268c37e61f191c045c037
matheemooksombut-dotcom <matheemooksombut@gmail.com>
2026-02-18 14:07:41 +0700

    updated

--- script.js
+++ script.js
@@ -34,3 +34,78 @@ dots.forEach(dot =>{
     })
 })
 
+// ===== LOGIN =====
+  const cartIcon = document.querySelector('.cart-icon');
+  const userIcon = document.querySelector('.user-icon');
+  const loginModal = document.querySelector('.login-modal');
+  const overlay = document.querySelector('.overlay');
+  const loginClose = document.querySelector('.login-modal .close');
+  const btnbuysection = document.getElementById('loginsection-btn-buy');
+
+  const btnbuy = document.querySelector('.btn-buy');
+
+
+  function openLoginModal() {
+    loginModal?.classList.add('active');
+    overlay?.classList.add('active');
+  }
+
+  function closeLoginModal() {
+    loginModal?.classList.remove('active');
+    overlay?.classList.remove('active');
+  }
+  cartIcon?.addEventListener('click', openLoginModal);
+  userIcon?.addEventListener('click', openLoginModal);
+  btnbuy?.addEventListener('click', openLoginModal);
+  btnbuysection?.addEventListener('click', openLoginModal);
+  loginClose?.addEventListener('click', closeLoginModal);
+
+
+  // ===== LOGOUT =====
+  const userLogout = document.querySelector('.user-logout');
+  const logoutModal = document.querySelector('.logout-modal');
+  const logoutClose = document.querySelector('.logout-modal .close');
+
+  function openLogoutModal() {
+    logoutModal?.classList.add('active');
+    overlay?.classList.add('active');
+  }
+
+  function closeLogoutModal() {
+    logoutModal?.classList.remove('active');
+    overlay?.classList.remove('active');
+  }
+
+  userLogout?.addEventListener('click', openLogoutModal);
+  logoutClose?.addEventListener('click', closeLogoutModal);
+
+
+  // ===== overlay close =====
+  overlay?.addEventListener('click', () => {
+    closeLoginModal();
+    closeLogoutModal();
+  });
+
+const plusbtn =document.querySelector('.plus')
+const minusbtn =document.querySelector('.minus')
+const qty = document.querySelector('.qty')
+
+
+plusbtn.addEventListener('click', function(){
+    qty.value++
+
+
+})
+
+
+minusbtn.addEventListener('click', function(){
+    if(qty.value > 1){
+        qty.value--
+    }
+
+})
+
+
+
+
+  
